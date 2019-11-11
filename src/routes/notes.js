e6dce63886ce0f628e71b9ced5af2160e10c3386
Introxucs <57341667+Introxucs@users.noreply.github.com>
2019-11-10 21:31:20 -0400

    Ultima Version VIEWS
    codigo para que se utilice en el backen
    (te amo)

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res)=>{
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', (req, res) =>{
+    const {nombre, description, electrodomestico, comuna, number, direction} = req.body;
+    res.send('ok')
+});
+
 router.get('/notes', (req, res) =>{
-    res.send('Notes from database');
+    res.render('notes/all-notes');
+});
+
+router.get('/notes/edit/:id', async (req, res)=>{
+    await Note.findById(req.params.id);
+    res.render('note/edit-note', {note});
+});
+
+router.put('/notes/edit-note/:id', (req, res) =>{
+
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect('/notes');
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,9 +10,35 @@ router.get('/users/signup', (req, res) =>{
 });
 
 router.post('/users/signup', (req, res) =>{
-    console.log(req.body);
-    res.send('ok')
+    const{ name, email, password, confirm_password} = req.body;
+    const errors = [];
+    if(name.length <= 0){
+        errors.push({text: 'Por favor inserte un nombre'});
+    }
+    if(email.length <= 0){
+        errors.push({text: 'Por favor inserte un email'});
+    }
+    if(password != confirm_password){
+        errors.push({text: 'Contrasenas no coinciden'});
+    }
+    if(password.length <=0){
+        errors.push({text: 'Por favor inserte contrasena'})
+    }
+    if(password.length < 4){
+        errors.push({ text: 'Contrasena menos a 4 caracteres'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup', {errors, name, email, password, confirm_password});
+    }else{
+        res.send('ok')
+    }
+    
+
 });
 
+router.get('/users/logout', (req, res) =>{
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
